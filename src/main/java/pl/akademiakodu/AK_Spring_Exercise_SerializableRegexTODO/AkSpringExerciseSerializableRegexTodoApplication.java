d90d5275ee90a41fe2ee06d711673873bf1f672a
Kerawos <Maresowa@gmail.com>
2017-11-22 10:51:17 +0100

    Initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_SerializableRegexTODO/AkSpringExerciseSerializableRegexTodoApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.AK_Spring_Exercise_SerializableRegexTODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpringExerciseSerializableRegexTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseSerializableRegexTodoApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/akademiakodu/AK_Spring_Exercise_SerializableRegexTODO/AkSpringExerciseSerializableRegexTodoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.AK_Spring_Exercise_SerializableRegexTODO;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkSpringExerciseSerializableRegexTodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
